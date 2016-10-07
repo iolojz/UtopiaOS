8a598c1655f663e902067cb38bc8a131e9831509
iolojz <iolojz.github@gmail.com>
2016-10-07 12:20:48 +0200

    Finished stubs for cross-compilation support for now.

--- src/target/x86-64.cmake
+++ src/target/x86-64.cmake
@@ -1,6 +1,6 @@
 # At the very least, these fields have to be set
 
-set (CMAKE_SYSTEM_NAME UtopiaOS)
+set (CMAKE_SYSTEM_NAME Generic)
 set (CMAKE_SYSTEM_VERSION 1)
 set (CMAKE_SYSTEM_PROCESSOR x86-64)
 set (CMAKE_C_COMPILER)
